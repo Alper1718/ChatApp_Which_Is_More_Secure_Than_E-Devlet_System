e4cccf395e15753ef91d1d70a2247c38c7f4f2ef
Alper1718 <104254640+Alper1718@users.noreply.github.com>
2024-07-24 12:22:09 +0300

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client implements Runnable {
+
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
+
+    @Override
+    public void run() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter IP:");
+            String IP = scanner.nextLine();
+
+            client = new Socket(IP, 3169);
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            System.out.println("Enter your nickname:");
+            String nickname = scanner.nextLine();
+            out.println(nickname);
+
+            InputHandler inHandler = new InputHandler();
+            Thread t = new Thread(inHandler);
+            t.start();
+
+            String inMessage;
+            while ((inMessage = in.readLine()) != null) {
+                System.out.println(inMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            shutdown();
+        }
+    }
+
+    public void shutdown() {
+        done = true;
+        try {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (client != null && !client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class InputHandler implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message = inReader.readLine();
+                    if (message.equals("/quit")) {
+                        shutdown();
+                    } else {
+                        out.println(message);
+                    }
+                }
+
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+}
